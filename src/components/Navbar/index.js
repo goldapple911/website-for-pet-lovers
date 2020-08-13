1634121f2deccf011f60da99b0fa7ae33298478b
kevin <goldapple820@gmail.com>
2020-08-12 21:17:39 -0500

    Reorganized stylesheets

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,47 +1,56 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import '../../pages/style.css'
+import React from "react"
+import { Link, useLocation } from "react-router-dom"
+import "../../pages/style.css"
+import logo from "../../pages/logo.png"
+import "./style.css"
 
 function NavTabs() {
 
     const location = useLocation();
 
     return (
-        <nav className="nav-background">
-            <ul className="nav nav-links">
-                <li className="nav-item">
-                    <Link
-                        to="/"
-                        className={location.pathname === "/" ? "nav-link active-link" : "nav-link inactive-link"}
-                    >
-                        Home
+        <nav>
+            <div className="nav-background">
+                <ul className="nav nav-links">
+                    <div className="col-md-3">
+                    <img src={logo} alt="logo" className="logo" />
+                    </div>
+                    <div className="nav col-md-9">
+                    <li className="nav-item">
+                        <Link
+                            to="/"
+                            className={location.pathname === "/" ? "nav-link active-link" : "nav-link inactive-link"}
+                        >
+                            Home
                     </Link>
-                </li>
-                <li className="nav-item">
-                    <Link
-                        to="/ShopLocal"
-                        className={location.pathname === "/ShopLocal" ? "nav-link active-link" : "nav-link inactive-link"}
-                    >
-                        Shop Atlas
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/ShopLocal"
+                            className={location.pathname === "/ShopLocal" ? "nav-link active-link" : "nav-link inactive-link"}
+                        >
+                            Shop Atlas
                     </Link>
-                </li>
-                <li className="nav-item">
-                    <Link
-                        to="/Contact"
-                        className={location.pathname === "/Contact" ? "nav-link active-link" : "nav-link inactive-link"}
-                    >
-                        Contact Us
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/Contact"
+                            className={location.pathname === "/Contact" ? "nav-link active-link" : "nav-link inactive-link"}
+                        >
+                            Contact Us
                     </Link>
-                </li>
-                <li className="nav-item">
-                    <Link
-                        to="/Careers"
-                        className={location.pathname === "/Careers" ? "nav-link active-link" : "nav-link inactive-link"}
-                    >
-                        Careers
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/Careers"
+                            className={location.pathname === "/Careers" ? "nav-link active-link" : "nav-link inactive-link"}
+                        >
+                            Careers
                     </Link>
-                </li>
-            </ul>
+                    </li>
+                    </div>
+                </ul>
+            </div>
         </nav>
     )
 }
