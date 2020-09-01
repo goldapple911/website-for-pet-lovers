6640e44446e28fc25b5049d06d9682405cb4b4c7
kevin <goldapple820@gmail.com>
2020-09-01 15:18:55 -0500

    Homepage tweaks

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -12,42 +12,42 @@ function NavTabs() {
         <nav>
             <div className="nav-background">
                 <ul className="nav nav-links">
-                    <div className="col-md-3">
-                    <img src={logo} alt="logo" className="logo" />
+                    <div className="col-md-4">
+                        <img src={logo} alt="logo" className="logo" />
                     </div>
-                    <div className="nav col-md-9">
-                    <li className="nav-item">
-                        <Link
-                            to="/"
-                            className={location.pathname === "/" ? "nav-link active-link" : "nav-link inactive-link"}
-                        >
-                            Home
-                    </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link
-                            to="/ShopLocal"
-                            className={location.pathname === "/ShopLocal" ? "nav-link active-link" : "nav-link inactive-link"}
-                        >
-                            Shop Atlas
-                    </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link
-                            to="/Contact"
-                            className={location.pathname === "/Contact" ? "nav-link active-link" : "nav-link inactive-link"}
-                        >
-                            Contact Us
-                    </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link
-                            to="/Careers"
-                            className={location.pathname === "/Careers" ? "nav-link active-link" : "nav-link inactive-link"}
-                        >
-                            Careers
-                    </Link>
-                    </li>
+                    <div className="nav col-md-8">
+                        <li className="nav-item">
+                            <Link
+                                to="/"
+                                className={location.pathname === "/" ? "nav-link active-link" : "nav-link inactive-link"}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/ShopLocal"
+                                className={location.pathname === "/ShopLocal" ? "nav-link active-link" : "nav-link inactive-link"}
+                            >
+                                Shop Atlas
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/Contact"
+                                className={location.pathname === "/Contact" ? "nav-link active-link" : "nav-link inactive-link"}
+                            >
+                                Contact Us
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/Careers"
+                                className={location.pathname === "/Careers" ? "nav-link active-link" : "nav-link inactive-link"}
+                            >
+                                Careers
+                            </Link>
+                        </li>
                     </div>
                 </ul>
             </div>
