5226316bda1821ae8f7ec784082ef3c48c4245b9
kevin <goldapple820@gmail.com>
2020-09-04 17:05:19 -0500

    Created contact submission form

---
+++ client/src/components/ContactForm/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+function ContactForm() {
+
+    return (
+        <div className="jumbotron">
+                <form className="form">
+                    <h1 className="display-4 main-txt"><strong>Drop a Line!</strong></h1>
+                    <hr className="my-4" />
+                    <p className="lead main-txt">Questions, Comments, Concerns?</p>
+                    <p className="lead main-txt">Here's your chance to let us know!</p>
+                    <label htmlFor="name">Name</label>
+                    <input className="inputs" type="text" id="name" name="name"></input>
+                    <br />
+                    <label htmlFor="email">Email</label>
+                    <input className="inputs" type="text" id="email" name="email"></input>
+                    <br />
+                    <label htmlFor="message">Message</label>
+                    <textarea className="inputs" rows="6" id="message" name="message"></textarea>
+                    <input type="submit" value="Send Message"></input>
+                </form>
+                <div className="row-overlay"></div>
+        </div>
+    )
+}
+
+
+export default ContactForm;
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import video from './assets/dogEating.mp4'
 import Background from '../components/Background'
+import ContactForm from '../components/ContactForm'
 
 export default function index() {
     return (
         <main>
         <Background src={video}/>
+        <div className="col-md-6">
+
+        </div>
+        <div className="col-md-6">
+            <ContactForm />
+        </div>
         </main>
     )
 }
