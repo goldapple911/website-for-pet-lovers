8e782e9427914f5fcdc97e8eccbc72acf63f8550
kevin <goldapple820@gmail.com>
2020-09-08 11:20:17 -0500

    Created dropdown for nav and added logo link

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -14,9 +14,9 @@ function NavTabs() {
     return (
         <nav>
             <div className="nav-background">
-                <div className="nav nav-links link-wrapper">
+                <div className="nav nav-links nav-wrapper">
                     <div className="col-md-4">
-                        <img src={logo} alt="logo" className="logo" />
+                        <a href="/"><img src={logo} alt="logo" className="logo" /></a>
                     </div>
                     <div className="col-md-8">
                         <Link
@@ -25,18 +25,22 @@ function NavTabs() {
                         >
                             Home
                             </Link>
-                        <Link
-                            to="/Shop-Local"
-                            className={location.pathname === "/Shop-Local" ? "nav-link active-link" : "nav-link inactive-link"}
-                        >
+
+                        <div className={"nav-link inactive-link d-down"}>
                             Shop Atlas
-                            </Link>
+                            <div className="dropdown">
+                                <a className="dropdown-link" href="/Shop-Local">In Store</a>
+                                <a className="dropdown-link" href="https://shop.atlaspet.net/products/list/" target="_blank">Online</a>
+                            </div>
+                        </div>
+
                         <Link
                             to="/Contact"
                             className={location.pathname === "/Contact" ? "nav-link active-link" : "nav-link inactive-link"}
                         >
                             Contact Us
                             </Link>
+
                         <Link
                             to="/Careers"
                             className={location.pathname === "/Careers" ? "nav-link active-link" : "nav-link inactive-link"}
