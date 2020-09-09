fd2fbc0e8dc782260474f4ec8d937a88be621f29
kevin <goldapple820@gmail.com>
2020-09-09 12:11:30 -0500

    Added starting coordinates to map

--- client/src/components/LocatorMap/index.js
+++ client/src/components/LocatorMap/index.js
@@ -7,10 +7,10 @@ mapboxgl.accessToken = "pk.eyJ1IjoibmF0aGFuc3p1IiwiYSI6ImNrZXVhaDl0ZzE4d3kyd3NhO
 
 const LocatorMap = () => {
     const mapContainerRef = useRef(null);
-  
-    const [lng, setLng] = useState(5);
-    const [lat, setLat] = useState(34);
-    const [zoom, setZoom] = useState(1.5);
+    
+    const [lng, setLng] = useState(-93.2334946);
+    const [lat, setLat] = useState(45.2019585);
+    const [zoom, setZoom] = useState(15);
   
     // Initialize map when component mounts
     useEffect(() => {
@@ -20,6 +20,56 @@ const LocatorMap = () => {
         center: [lng, lat],
         zoom: zoom
       });
+
+
+    //   TESTING GeoJSON
+    var geojson = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.034084142948,
+                38.909671288923
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(202) 234-7336",
+              "phone": "2022347336",
+              "address": "1471 P St NW",
+              "city": "Washington DC",
+              "country": "United States",
+              "crossStreet": "at 15th St NW",
+              "postalCode": "20005",
+              "state": "D.C."
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.049766,
+                38.900772
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(202) 507-8357",
+              "phone": "2025078357",
+              "address": "2221 I St NW",
+              "city": "Washington DC",
+              "country": "United States",
+              "crossStreet": "at 22nd St NW",
+              "postalCode": "20037",
+              "state": "D.C."
+            }
+          }
+        ]
+      };
+
+    //   END GeoJSON test
   
       // Add navigation control (the +/- zoom buttons)
     //   map.addControl(new mapboxgl.NavigationControl(), 'top-right');
