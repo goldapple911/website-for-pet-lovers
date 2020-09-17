774b2c62e63c4c1c8056ff87a18247688bf32e7d
kevin <goldapple820@gmail.com>
2020-09-17 08:31:56 -0500

    Tweaking path for catch all route

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Catch all route to allow React to function on page
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'client/public/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
