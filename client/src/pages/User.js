dffbec5bfde8d201a0d23e4d0d270aa32f571b1f
kevin <goldapple820@gmail.com>
2020-11-12 10:43:07 -0600

    Added code to send component based on user role

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -6,6 +6,8 @@ import JobCard from '../components/CardJob';
 import app from '../utils/firebase';
 import Application from '../components/Application'
 import { AuthContext } from '../utils/AuthContext';
+import firebase from 'firebase/app';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 // import * as firebase from 'firebase/app';
 
 
@@ -14,13 +16,38 @@ export default function User() {
     let currentPositionsBlaine = [];
     let currentPositionsStillwater = [];
 
+    var db = firebase.firestore(app)
+
+    const managerRef = db.collection("managers")
+
+    const [managerList] = useCollectionData(managerRef)
+    // console.log(managerList)
+
+
     const [apply, setApply] = useState(false);
     const { currentUser } = useContext(AuthContext);
     const [position, setPosition] = useState();
+    const [isManager, setIsManager] = useState(false);
+    const [managerLocation, setManagerLocation] = useState("")
+
+    const checkForManager = (managerList) => {
+        // For loop to check currentUser for manager status
+        if (managerList) {
+            for (let x = 0; x < managerList.length; x++) {
+                if (managerList[x].uid === currentUser.uid) {
+                    setIsManager(true)
+                    console.log("success")
+                }
+
+            }
+        }
+    }
 
     const [editUser, setEditUser] = useState(false);
 
     useEffect(() => {
+
+        // For loop to populate state with current job openings
         // Read all potential jobdata
         for (let i = 0; i < jobData.alljobs.length; i++) {
             // Check to see if the job is currently hiring
@@ -72,31 +99,36 @@ export default function User() {
                 <div className="col-md-12 job-section user-page">
                     <div className="job-section-overlay"></div>
                     <div className="row">
-                        <div className="col-md-12 text-center job-header">   
+                        <div className="col-md-12 text-center job-header">
                             <button className="btn btn-light btn-filter sign-out" onClick={() => app.auth().signOut()}>Sign Out</button>
-                            <button className="btn btn-light btn-filter edit" onClick={() => {if(editUser){setEditUser(false)} else{setEditUser(true)}}} >Account</button>
+                            <button className="btn btn-light btn-filter edit" onClick={() => { if (editUser) { setEditUser(false) } else { setEditUser(true) } }} >Account</button>
                         </div>
                     </div>
 
+                    {/* Conditional statement to return back end user page to view submitted applications if user is on the manager list */}
+                    {isManager ? <h1>Manager Yo</h1> : <>
 
-                    {apply ? <Application position={position} currentUser={currentUser} setApply={setApply}/> :
-                        <div className="col-md-12 text-center">
-                            <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="all">All Locations</button>
-                            <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="Blaine">Blaine</button>
-                            <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="Stillwater">Stillwater</button>
+                        {/* Conditional statement to return the application form or open position search based on state changes */}
+                        {apply ? <Application position={position} currentUser={currentUser} setApply={setApply} /> :
+                            <div className="col-md-12 text-center">
+                                <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="all">All Locations</button>
+                                <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="Blaine">Blaine</button>
+                                <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="Stillwater">Stillwater</button>
 
-                            <div className="row">
-                                {jobs.map((job) => (
-                                    <div className="col-lg-3 col-md-4 col-sm-6" key={job.id}>
-                                        <JobCard
-                                            job={job}
-                                        />
-                                    <div className="job-overlay" onClick={(e) => { setApply(true); setPosition(e.target.id) }} id={job.title + ": " + job.location}></div>
-                                    </div>
-                                ))}
+                                <div className="row">
+                                    {jobs.map((job) => (
+                                        <div className="col-lg-3 col-md-4 col-sm-6" key={job.id}>
+                                            <JobCard
+                                                job={job}
+                                            />
+                                            <div className="job-overlay" onClick={(e) => { setApply(true); setPosition(e.target.id) }} id={job.title + ": " + job.location}></div>
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                    }
+                        }
+                    </>}
+
 
                 </div>
             </div>
