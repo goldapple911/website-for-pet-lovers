dccc42cd01d6687450e0fe2729a4697eff7a8abd
kevin <goldapple820@gmail.com>
2020-11-19 10:32:16 -0600

    Moved elements within useEffect hook so they only run once on component mount

---
+++ client/src/components/ManagerDisplay/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+
+export default function index() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -7,7 +7,8 @@ import app from '../utils/firebase';
 import Application from '../components/Application'
 import { AuthContext } from '../utils/AuthContext';
 import firebase from 'firebase/app';
-import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import ManagerDisplay from '../components/ManagerDisplay'
 // import * as firebase from 'firebase/app';
 
 
@@ -18,32 +19,32 @@ export default function User() {
 
     var db = firebase.firestore(app)
 
-    const managerRef = db.collection("managers")
-
-    db.collection("managers")
-    .get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(doc.data().uid)
-            if (doc.data().uid === currentUser.uid) {
-                setIsManager(true)
-            } else {
-                
-            }
-        })
-    })
-
-
     const [apply, setApply] = useState(false);
     const { currentUser } = useContext(AuthContext);
     const [position, setPosition] = useState();
     const [isManager, setIsManager] = useState(false);
     const [managerLocation, setManagerLocation] = useState("")
 
+
+
     const [editUser, setEditUser] = useState(false);
 
     useEffect(() => {
-        
+
+        // Sets a user's account level after cross referencing the manager list in the database.
+        db.collection("managers")
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    console.log(doc.data().uid)
+                    if (doc.data().uid === currentUser.uid) {
+                        setIsManager(true)
+                    } else {
+
+                    }
+                })
+            })
+
         // For loop to populate state with current job openings
         // Read all potential jobdata
         for (let i = 0; i < jobData.alljobs.length; i++) {
@@ -104,27 +105,27 @@ export default function User() {
                     </div>
 
                     {/* Conditional statement to return back end user page to view submitted applications if user is on the manager list */}
-                    {isManager ? <h1>Manager Yo</h1> : <>
+                    {isManager ? <ManagerDisplay /> : <>
 
-                        {/* Conditional statement to return the application form or open position search based on state changes */}
-                        {apply ? <Application position={position} currentUser={currentUser} setApply={setApply} /> :
-                            <div className="col-md-12 text-center">
-                                <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="all">All Locations</button>
-                                <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="Blaine">Blaine</button>
-                                <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="Stillwater">Stillwater</button>
+                    {/* Conditional statement to return the application form or open position search based on state changes */}
+                    {apply ? <Application position={position} currentUser={currentUser} setApply={setApply} /> :
+                        <div className="col-md-12 text-center">
+                            <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="all">All Locations</button>
+                            <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="Blaine">Blaine</button>
+                            <button type="button" className="btn btn-light btn-filter" onClick={handleLocationChange} value="Stillwater">Stillwater</button>
 
-                                <div className="row">
-                                    {jobs.map((job) => (
-                                        <div className="col-lg-3 col-md-4 col-sm-6" key={job.id}>
-                                            <JobCard
-                                                job={job}
-                                            />
-                                            <div className="job-overlay" onClick={(e) => { setApply(true); setPosition(e.target.id) }} id={job.title + ": " + job.location}></div>
-                                        </div>
-                                    ))}
-                                </div>
+                            <div className="row">
+                                {jobs.map((job) => (
+                                    <div className="col-lg-3 col-md-4 col-sm-6" key={job.id}>
+                                        <JobCard
+                                            job={job}
+                                        />
+                                        <div className="job-overlay" onClick={(e) => { setApply(true); setPosition(e.target.id) }} id={job.title + ": " + job.location}></div>
+                                    </div>
+                                ))}
                             </div>
-                        }
+                        </div>
+                    }
                     </>}
 
 
