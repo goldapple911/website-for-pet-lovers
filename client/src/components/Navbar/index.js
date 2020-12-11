9f78d6b750d0aba1202ef8f7bd4b09f96f1218ee
kevin <goldapple820@gmail.com>
2020-12-11 09:34:09 -0600

    Altered navbar styling for increased performance

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -11,44 +11,34 @@ function NavTabs() {
     return (
         <nav className="print-hide">
             <div className="nav-background">
-                <div className="nav nav-links nav-wrapper">
+                <div className="nav nav-links ml-auto mr-auto row">
+
                     <div className="col-md-4">
+                        {/* Logo sends to home when clicked */}
                         <Link to="/"><img src={logo} alt="logo" className="logo" /></Link>
                     </div>
-                    <div className="col-md-8">
-                        <Link
-                            to="/"
-                            className={location.pathname === "/" ? "nav-link active-link" : "nav-link inactive-link"}
-                        >
-                            Home
-                            </Link>
 
+                    <div className="col-md-8 links">
+                        {/* Home link */}
+                        <Link to="/" className={location.pathname === "/" ? "nav-link active-link" : "nav-link inactive-link"}>Home</Link>
+
+                        {/* Dropdown menu that shows links to shop in store or online */}
                         <div className={location.pathname === "/Shop-Local" ? "nav-link active-link d-down" : "nav-link inactive-link d-down"}>
                             Shop Atlas
-                            <div className="dropdown">
-                                <Link
-                                    to="/Shop-Local"
-                                    className="dropdown-link"
-                                >
-                                    In Store
-                                </Link>
+
+                            <div className="dropdown text-center">
+                                {/* Shop in store */}
+                                <Link to="/Shop-Local" className="dropdown-link">In Store</Link>
+                                {/* Shop online */}
                                 <a className="dropdown-link" href="https://shop.atlaspet.net/products/list/" target="_blank" rel="noopener noreferrer">Online</a>
                             </div>
+
                         </div>
 
-                        {/* <Link
-                            to="/Contact"
-                            className={location.pathname === "/Contact" ? "nav-link active-link" : "nav-link inactive-link"}
-                        >
-                            Contact Us
-                            </Link> */}
+                        {/* <Link to="/Contact" className={location.pathname === "/Contact" ? "nav-link active-link" : "nav-link inactive-link"}>Contact Us</Link> */}
 
-                        <Link
-                            to="/Careers"
-                            className={location.pathname === "/Careers" ? "nav-link active-link" : "nav-link inactive-link"}
-                        >
-                            Careers
-                            </Link>
+                        {/* Careers page link */}
+                        <Link to="/Careers" className={location.pathname === "/Careers" ? "nav-link active-link" : "nav-link inactive-link"}>Careers</Link>
 
                     </div>
                 </div>
